e51afe26638878d4e96cf90820f27a56aa974988
Quentin Young <qlyoung@qlyoung.net>
2016-06-05 19:41:23 -0400

    added more tests

--- tests/test.c
+++ tests/test.c
@@ -62,7 +62,10 @@ void test_make_hid_report_one_char() {
     char id  = lo->map[i]->id;
     char mod = lo->map[i]->mod;
 
-    make_hid_report(report, 0, 1, lo->map[i]->ch);
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 1, codepoint);
     TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
     TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
     TEST_ASSERT_EQUAL(id,   report[2]); // usage id
@@ -74,11 +77,143 @@ void test_make_hid_report_one_char() {
   }
 }
 
+void test_make_hid_report_two_chars() {
+  // test report generation for all codepoints in layout
+  for (int i = 0; i < lo->size; i++) {
+    // clear report
+    memset(report, 0x00, (size_t) 8);
+
+    // retrieve expected values
+    char id  = lo->map[i]->id;
+    char mod = lo->map[i]->mod;
+
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 2, codepoint, codepoint);
+    TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
+    TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
+    TEST_ASSERT_EQUAL(id,   report[2]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[3]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[4]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[5]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[6]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[7]); // usage id
+  }
+}
+
+
+void test_make_hid_report_three_chars() {
+  // test report generation for all codepoints in layout
+  for (int i = 0; i < lo->size; i++) {
+    // clear report
+    memset(report, 0x00, (size_t) 8);
+
+    // retrieve expected values
+    char id  = lo->map[i]->id;
+    char mod = lo->map[i]->mod;
+
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 3, codepoint, codepoint, codepoint);
+    TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
+    TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
+    TEST_ASSERT_EQUAL(id,   report[2]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[3]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[4]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[5]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[6]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[7]); // usage id
+  }
+}
+
+void test_make_hid_report_four_chars() {
+  // test report generation for all codepoints in layout
+  for (int i = 0; i < lo->size; i++) {
+    // clear report
+    memset(report, 0x00, (size_t) 8);
+
+    // retrieve expected values
+    char id  = lo->map[i]->id;
+    char mod = lo->map[i]->mod;
+
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 4, codepoint, codepoint, codepoint, codepoint);
+    TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
+    TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
+    TEST_ASSERT_EQUAL(id,   report[2]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[3]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[4]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[5]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[6]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[7]); // usage id
+  }
+}
+
+void test_make_hid_report_five_chars() {
+  // test report generation for all codepoints in layout
+  for (int i = 0; i < lo->size; i++) {
+    // clear report
+    memset(report, 0x00, (size_t) 8);
+
+    // retrieve expected values
+    char id  = lo->map[i]->id;
+    char mod = lo->map[i]->mod;
+
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 5, codepoint, codepoint, codepoint, codepoint, codepoint);
+    TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
+    TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
+    TEST_ASSERT_EQUAL(id,   report[2]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[3]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[4]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[5]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[6]); // usage id
+    TEST_ASSERT_EQUAL(0x00, report[7]); // usage id
+  }
+}
+
+void test_make_hid_report_six_chars() {
+  // test report generation for all codepoints in layout
+  for (int i = 0; i < lo->size; i++) {
+    // clear report
+    memset(report, 0x00, (size_t) 8);
+
+    // retrieve expected values
+    char id  = lo->map[i]->id;
+    char mod = lo->map[i]->mod;
+
+    // get codepoint to encode
+    uint32_t codepoint = lo->map[i]->ch;
+
+    make_hid_report(report, 0, 6, codepoint, codepoint, codepoint, codepoint, codepoint, codepoint);
+    TEST_ASSERT_EQUAL(mod,  report[0]); // modifier
+    TEST_ASSERT_EQUAL(0x00, report[1]); // reserved
+    TEST_ASSERT_EQUAL(id,   report[2]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[3]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[4]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[5]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[6]); // usage id
+    TEST_ASSERT_EQUAL(id,   report[7]); // usage id
+  }
+}
+
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_layout_file_loaded);
   RUN_TEST(test_all_fail_when_no_layout_set);
   RUN_TEST(test_make_hid_report_arr_nullargs_fails);
   RUN_TEST(test_make_hid_report_one_char);
+  RUN_TEST(test_make_hid_report_two_chars);
+  RUN_TEST(test_make_hid_report_three_chars);
+  RUN_TEST(test_make_hid_report_four_chars);
+  RUN_TEST(test_make_hid_report_five_chars);
+  RUN_TEST(test_make_hid_report_six_chars);
   return UNITY_END();
 }
